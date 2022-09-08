20bbaed4dbcf42646f9930f226e3601bd2a2d098
dino <dinodynamicpricing@gmail.com>
2022-09-08 16:26:04 +0300

    ..

--- web/frontend/components/ProductGroup.jsx
+++ web/frontend/components/ProductGroup.jsx
@@ -4,88 +4,94 @@
  * each group can add products
  */
 
-import createApp from "@shopify/app-bridge";
-import { Page, Card, DataTable, Button, Badge } from "@shopify/polaris";
+import { Page, Card, DataTable, Badge } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
-import React, { Component } from "react";
 import { useState } from "react";
 
 //data example
 
-class ProductGroup extends React.Component {
-	state = { open: false };
+export default function ProductGroup() {
+	const [open, setOpen] = useState(false);
+	const [rows, setRows] = useState([]);
 
-	render() {
-		// const [rows, setRows] = useState([]);
-		// setRows([
-		// 	[
-		// 		"Emerald Silk Gown",
-		// 		<Badge status="success" progress="complete" />,
-		// 		124689,
-		// 		140,
-		// 		"122",
-		// 	],
-		// 	[
-		// 		"Mauve Cashmere Scarf",
-		// 		<Badge status="success" progress="complete" />,
-		// 		124533,
-		// 		83,
-		// 		"90",
-		// 	],
-		// 	[
-		// 		"Navy Merino Wool Blazer with khaki chinos and yellow belt",
-		// 		<Badge status="success" progress="complete" />,
-		// 		124518,
-		// 		32,
-		// 		"14240",
-		// 	],
-		// ]);
-		return (
-			<Page
-				title="Product Groups"
-				primaryAction={{
-					content: "add products",
-					onAction: () => {
-						this.setState({ open: true });
-					},
-				}}
-			>
-				<ResourcePicker
-					resourceType="Product"
-					open={this.state.open}
-					onCancel={() => this.setState({ open: false })}
-					onSelection={(productsSelction) => {
-						this.handleProducts(productsSelction);
-					}}
-				/>
-				<Card>
-					<DataTable
-						columnContentTypes={[
-							"text",
-							"numeric",
-							"numeric",
-							"numeric",
-							"numeric",
-						]}
-						headings={[
-							"Group number 1",
-							"Status",
-							"Revenue",
-							"Days",
-							"Impression",
-						]}
-						rows={[[]]}
-						totals={["", "", "", 255, "8300"]}
-					/>
-				</Card>
-			</Page>
-		);
-	}
-	//handle the selected products
-	handleProducts = (productsSelction) => {
-		this.setState({ open: false });
-		console.log(productsSelction);
+	// const [rows, setRows] = useState([]);
+	// setRows([
+	// 	[
+	// 		"Emerald Silk Gown",
+	// 		<Badge status="success" progress="complete" />,
+	// 		124689,
+	// 		140,
+	// 		"122",
+	// 	],
+	// [
+	// 	"Mauve Cashmere Scarf",
+	// 	<Badge status="success" progress="complete" />,
+	// 	124533,
+	// 	83,
+	// 	"90",
+	// ],
+	// 	[
+	// 		"Navy Merino Wool Blazer with khaki chinos and yellow belt",
+	// 		<Badge status="success" progress="complete" />,
+	// 		124518,
+	// 		32,
+	// 		"14240",
+	// 	],
+	// ]);
+
+	/**
+	 * add product to product group
+	 * @param {*} p product from ResourcePicker
+	 */
+	const addProduct = (p) => {
+		setRows((rows) => [
+			...rows,
+			[p.handle, <Badge status="success" progress="complete" />, 0, 0, 0],
+		]);
 	};
-}
 
-export default ProductGroup;
+	return (
+		<Page
+			title="Product Groups"
+			primaryAction={{
+				content: "add products",
+				onAction: () => {
+					setOpen(true);
+				},
+			}}
+		>
+			<ResourcePicker
+				resourceType="Product"
+				open={open}
+				onCancel={() => setOpen(false)}
+				onSelection={(productsSelction) => {
+					setOpen(false);
+					productsSelction.selection.forEach((p) => {
+						addProduct(p);
+						// sent to the server and store in D.B
+					});
+				}}
+			/>
+			<Card>
+				<DataTable
+					columnContentTypes={[
+						"text",
+						"numeric",
+						"numeric",
+						"numeric",
+						"numeric",
+					]}
+					headings={[
+						"Group number 1",
+						"Status",
+						"Revenue",
+						"Days",
+						"Impression",
+					]}
+					rows={rows}
+					totals={["", "", "", "", ""]}
+				/>
+			</Card>
+		</Page>
+	);
+}
